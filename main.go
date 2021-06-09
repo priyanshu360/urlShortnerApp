429ca0c7a6876d33afa33c53e24163a782422483
priyanshu360 <priyanshurajput360@gmail.com>
2021-06-09 12:30:15 +0530

    Improve some formatting

--- main.go
+++ main.go
@@ -1,84 +1,63 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
-	"encoding/json"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    // "go.mongodb.org/mongo-driver/mongo/readpref"
-    // "time"
-    "context"
-    "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
 )
 
-
-type Url struct{
-	Id string `json:"id" binding:"required"`
-	LongUrl string `json:"long_url" binding:"required"`
+type Url struct {
+	Id       string `json:"id" binding:"required"`
+	LongUrl  string `json:"long_url" binding:"required"`
 	ShortUrl string `json:"short_url" binding:"required"`
 }
 
 var db *mongo.Collection
 var ctx = context.TODO()
 
-
-func init(){
+func init() {
 	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017/")
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.Connect(ctx, clientOptions)
+	errorHandler(err)
 
-    err = client.Ping(ctx, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Ping(ctx, nil)
+	errorHandler(err)
 
-    db = client.Database("urlShorterDB").Collection("urls")
+	db = client.Database("urlShorterDB").Collection("urls")
 }
 
-
-
-
-func main(){
+func main() {
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", MainPageHandler)
 	router.HandleFunc("/create-short-url", LongUrlHandler).Methods("POST")
 	router.HandleFunc("/{shortUrl}", ShortUrlHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
-
-func MainPageHandler(w http.ResponseWriter, r *http.Request){
+func MainPageHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-
-
-func LongUrlHandler(w http.ResponseWriter, r *http.Request){
+func LongUrlHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	var uid string
+	cnt, _ := db.CountDocuments(ctx, bson.M{"longurl": r.FormValue("url")})
 
-	cnt, _ := db.CountDocuments(ctx, bson.M{"longurl":r.FormValue("url")})
-
-	if cnt != 0{
+	if cnt != 0 {
 		var url []Url
-		cursor, err := db.Find(ctx, bson.M{"longurl":r.FormValue("url")})
+		cursor, err := db.Find(ctx, bson.M{"longurl": r.FormValue("url")})
+		errorHandler(err)
 
-
-		if err != nil {
-		    log.Fatal(err)
-		}
-		if err = cursor.All(ctx, &url); err != nil {
-		    log.Fatal(err)
-		}
-
+		err = cursor.All(ctx, &url)
+		errorHandler(err)
 
 		fmt.Println(r.FormValue("url"))
 		json.NewEncoder(w).Encode(url[0])
@@ -88,57 +67,48 @@ func LongUrlHandler(w http.ResponseWriter, r *http.Request){
 	for {
 		uid = uuid.New().String()[:5]
 		fmt.Println(uid)
-		cnt, _ := db.CountDocuments(ctx, bson.M{"id" : uid})
-		if cnt == 0{
+		cnt, _ := db.CountDocuments(ctx, bson.M{"id": uid})
+		if cnt == 0 {
 			break
 		}
 	}
 
 	host := "127.0.0.1:8081/"
 	url := Url{
-		Id : uid,
-		LongUrl : r.FormValue("url"),
-		ShortUrl : host + uid,
+		Id:       uid,
+		LongUrl:  r.FormValue("url"),
+		ShortUrl: host + uid,
 	}
 
 	res, err := db.InsertOne(ctx, url)
-	if err != nil {
-		log.Fatal(err)
-	}
+	errorHandler(err)
 
 	fmt.Printf("inserted document with ID %v\n", res.InsertedID)
 	json.NewEncoder(w).Encode(url)
 }
 
-
-
-func ShortUrlHandler(w http.ResponseWriter, r *http.Request){
+func ShortUrlHandler(w http.ResponseWriter, r *http.Request) {
 	urlId := mux.Vars(r)["shortUrl"]
-	fmt.Println("GET params were:", urlId)
 
-	cnt, err := db.CountDocuments(ctx, bson.M{"id" : urlId})
+	cnt, err := db.CountDocuments(ctx, bson.M{"id": urlId})
+	errorHandler(err)
 
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	if cnt == 0{
+	if cnt == 0 {
 		return
 	}
 
 	var url []Url
-	cursor, err := db.Find(ctx, bson.M{"id" : urlId})
+	cursor, err := db.Find(ctx, bson.M{"id": urlId})
+	errorHandler(err)
 
+	err = cursor.All(ctx, &url)
+	errorHandler(err)
 
-	if err != nil {
-	    log.Fatal(err)
-	}
-	if err = cursor.All(ctx, &url); err != nil {
-	    log.Fatal(err)
-	}
-
-	fmt.Println(url[0])
 	http.Redirect(w, r, url[0].LongUrl, 307)
+}
 
-
+func errorHandler(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
